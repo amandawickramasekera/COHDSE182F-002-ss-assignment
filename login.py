da2c468f7db14feae9fb70d84796c1057caf2d5b
Amanda Wickramasekera <andrewwickramasekera@gmail.com>
2018-11-16 09:41:42 -0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,11 @@
+un = input("Enter username")
+pw = input("Enter password")
+
+if(un == "" & pw == ""):
+
+	print("You have logged in successfully")
+
+else:
+
+	print("Incorrect username or password")
+
